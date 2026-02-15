a33bf959bf9a70d5ac82421a3d9f2cc43fd9f50d
sslakmon <shlomoslakmon@gmail.com>
2026-02-16 00:42:26 +0200

    lab 3 docker env upload

---
+++ lab 3 - volume attach/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Lab 3 - Volume')
+})
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Example app listening on port ${port}`)
+})
